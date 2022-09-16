394236124b96196e8df3891d753fbb0044e95f15
YuchenJiaa <yuchenjiacc@gmail.com>
2022-09-16 16:33:59 -0400

    Add project files.

---
+++ COMP2084Lab1/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace COMP2084Lab1.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
